c45e1d6ca5d90ed91ee6376a8a523cb61556d9f6
SgtStarshine <starshine621@ymail.com>
2019-01-26 12:28:56 -0500

    updated game html and js files, and updated api routes

--- data/game.js
+++ data/game.js
@@ -1,5 +1,14 @@
-var gameArray = [
-
-];
+var gameArray = [{
+    "eventname": "You are successful in your task and are revered as an awesome worker!",
+    "reputation": 10,
+    "knowledge": 10,
+    "sanity": 10
+},
+{
+    "eventname": "You have failed in your task and bring shame to the workplace...",
+    "reputation": 2,
+    "knowledge": 2,
+    "sanity": 2
+}];
 
 module.exports = gameArray;
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -7,10 +7,28 @@
 
 // Requiring our models
 var db = require("../models");
+var gameData = require("../data/game")
 
 // Routes
 // =============================================================
 module.exports = function(app) {
+
+  // API GET requests
+  app.get("/api/game", function(req, res) {
+    res.json(gameData);
+  })
+
+  // API POST Requests
+  app.post("/api/game", function(req, res) {
+    if (tableData.length < 5) {
+      tableData.push(req.body);
+      res.json(true);
+    }
+    else {
+      waitListData.push(req.body);
+      res.json(false);
+    }
+  });
 
   // POST route for creating a new character at the start of a game
   app.post("/api/game", function(req, res) {
